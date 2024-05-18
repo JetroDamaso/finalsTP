df30a395e48d800eb67a6e6cc9e4db93177c2c4d
JetroDamaso <jetropapa@gmail.com>
2024-05-18 13:39:43 +0800

    login ui only

--- finalsTP/LoginWindow.Designer.cs
+++ finalsTP/LoginWindow.Designer.cs
@@ -32,66 +32,139 @@ namespace finalsTP
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginWindow));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textEmail = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.clickForgotPass = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // panel1
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(228, 21);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(86, 48);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.UseWaitCursor = true;
+            this.panel1.BackColor = System.Drawing.Color.Red;
+            this.panel1.Location = new System.Drawing.Point(24, 109);
+            this.panel1.Margin = new System.Windows.Forms.Padding(15, 3, 3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(211, 2);
+            this.panel1.TabIndex = 2;
             // 
-            // label1
+            // textEmail
             // 
-            this.label1.Font = new System.Drawing.Font("Nunito Sans Normal Black", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(13, 21);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(209, 48);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "LOGIN";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.label1.UseWaitCursor = true;
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.textEmail.BackColor = System.Drawing.SystemColors.Control;
+            this.textEmail.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textEmail.Font = new System.Drawing.Font("Nunito Sans Normal", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textEmail.ForeColor = System.Drawing.Color.Black;
+            this.textEmail.Location = new System.Drawing.Point(24, 90);
+            this.textEmail.Name = "textEmail";
+            this.textEmail.Size = new System.Drawing.Size(211, 18);
+            this.textEmail.TabIndex = 3;
+            this.textEmail.Text = "Email";
             // 
             // textBox1
             // 
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(57, 75);
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Nunito Sans Normal", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Black;
+            this.textBox1.Location = new System.Drawing.Point(24, 128);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(214, 26);
-            this.textBox1.TabIndex = 2;
+            this.textBox1.Size = new System.Drawing.Size(211, 18);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "Password";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Red;
+            this.panel2.Location = new System.Drawing.Point(24, 147);
+            this.panel2.Margin = new System.Windows.Forms.Padding(15, 3, 3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(211, 2);
+            this.panel2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Nunito Sans Normal Medium", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(24, 187);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(211, 32);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "LOG IN";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // clickForgotPass
+            // 
+            this.clickForgotPass.Font = new System.Drawing.Font("Nunito Sans Normal ExtraBold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clickForgotPass.ForeColor = System.Drawing.Color.Crimson;
+            this.clickForgotPass.Location = new System.Drawing.Point(135, 152);
+            this.clickForgotPass.Name = "clickForgotPass";
+            this.clickForgotPass.Size = new System.Drawing.Size(100, 20);
+            this.clickForgotPass.TabIndex = 7;
+            this.clickForgotPass.Text = "Forgot Password";
+            this.clickForgotPass.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Nunito Sans Normal", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Crimson;
+            this.label2.Location = new System.Drawing.Point(225, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 22);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "X";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(131, 57);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // LoginWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(327, 509);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(259, 239);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.clickForgotPass);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.textEmail);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.pictureBox1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "LoginWindow";
-            this.Text = "LoginWindow";
-            this.UseWaitCursor = true;
+            this.Text = "ITEM Login";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
         }
 
-        private System.Windows.Forms.TextBox textBox1;
-
-        private System.Windows.Forms.Label label1;
-
         private System.Windows.Forms.PictureBox pictureBox1;
+
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.Label clickForgotPass;
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+
+        private System.Windows.Forms.TextBox textEmail;
+
+        private System.Windows.Forms.Panel panel1;
 
         #endregion
     }
--- finalsTP/LoginWindow.cs
+++ finalsTP/LoginWindow.cs
@@ -9,10 +9,7 @@ namespace finalsTP
         {
             InitializeComponent();
         }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-            throw new System.NotImplementedException();
-        }
+        
+        
     }
 }
